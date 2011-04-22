a7448aa10ccc9f1cbecb523ef42b94be171a35ff
Donovan Hide <donny@sur.ly>
2011-04-22 19:45:54 +0100

    added django model

---
+++ superfastmatch/django/__init__.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Document(models.Model):
+    source = models.URLField(verify_exists=False,blank=True,null=True)
+    content = models.TextField(blank=False,null=False)
+    cleaned_content = models.TextField(blank=False,null=False)
