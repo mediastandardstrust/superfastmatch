12f316cc66517d04ce0488258c8d131175abb90f
Donovan Hide <donny@sur.ly>
2011-10-24 02:21:07 +0100

    libunwind for linux

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,8 @@
 mkdir external
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+	curl -L http://download.savannah.gnu.org/releases/libunwind/libunwind-1.0.1.tar.gz -o external/libunwind-1.0.1.tar.gz
+fi
 curl http://google-ctemplate.googlecode.com/files/ctemplate-1.0.tar.gz -o external/ctemplate-1.0.tar.gz
 curl http://google-perftools.googlecode.com/files/google-perftools-1.8.3.tar.gz -o external/google-perftools-1.8.3.tar.gz
 curl http://google-gflags.googlecode.com/files/gflags-1.6.tar.gz -o external/gflags-1.6.tar.gz
@@ -8,6 +12,9 @@ curl http://www.oberhumer.com/opensource/lzo/download/lzo-2.06.tar.gz -o externa
 svn checkout http://google-sparsehash.googlecode.com/svn/trunk/ external/google-sparsehash
 
 cd external && for i in *.tar.gz; do tar xzvf $i; done
+if [[ "$unamestr" == 'Linux' ]]; then
+	cd libunwind* && CFLAGS=-U_FORTIFY_SOURCE ./configure && make && sudo make install && cd ..
+fi
 cd ctemplate* && ./configure && make && sudo make install && cd ..
 cd google-perftools* && ./configure && make && sudo make install  && cd ..
 cd gflags* && ./configure && make && sudo make install  && cd ..
