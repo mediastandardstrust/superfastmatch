e8096cac5008ec1824ed533aa51e9b52ed1aaf2c
Donovan Hide <donny@sur.ly>
2011-12-19 03:40:53 +0000

    fix bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,8 +6,8 @@ fi
 curl http://google-ctemplate.googlecode.com/files/ctemplate-1.0.tar.gz -o external/ctemplate-1.0.tar.gz
 curl http://google-perftools.googlecode.com/files/google-perftools-1.8.3.tar.gz -o external/google-perftools-1.8.3.tar.gz
 curl http://google-gflags.googlecode.com/files/gflags-1.6.tar.gz -o external/gflags-1.6.tar.gz
-curl http://fallabs.com/kyotocabinet/pkg/kyotocabinet-1.2.72.tar.gz -o external/kyotocabinet-1.2.70.tar.gz
-curl http://fallabs.com/kyototycoon/pkg/kyototycoon-0.9.52.tar.gz -o external/kyototycoon-0.9.51.tar.gz
+curl http://fallabs.com/kyotocabinet/pkg/kyotocabinet-1.2.72.tar.gz -o external/kyotocabinet-1.2.72.tar.gz
+curl http://fallabs.com/kyototycoon/pkg/kyototycoon-0.9.52.tar.gz -o external/kyototycoon-0.9.52.tar.gz
 curl http://www.oberhumer.com/opensource/lzo/download/lzo-2.06.tar.gz -o external/lzo-2.06.tar.gz
 svn checkout http://google-sparsehash.googlecode.com/svn/trunk/ external/google-sparsehash
 
