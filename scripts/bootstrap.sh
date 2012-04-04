789a23de04b402d18c9ed20f2c06c122a29e03b8
Drew Vogel <dvogel@sunlightfoundation.com>
2012-04-04 14:57:09 -0400

    Run ldconfig at the end of bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -32,3 +32,4 @@ cd sparsehash* && ./configure && make && sudo make install && cd ..
 cd lzo-2.06 && ./configure && make && sudo make install && cd ..
 cd re2 && hg import ../../scripts/re2.diff -u superfastmatch -m "fix symbols" && make && sudo make install && cd ..
 cd ..
+sudo ldconfig
